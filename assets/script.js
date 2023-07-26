fd5b802cbb34d6179279312ba278bc8be7abb2a6
Crystal Green <crissybrown1992@icloud.com>
2023-07-26 12:58:22 -0400

    Worked with tutor and followed tutorial for quiz development

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,146 @@
-var headerEl = document.querySelector(".pageheaer");
-var buttonEl = document.querySelector(".button");
-var mainpageEl = document.querySelector(".mainpage");
-var questionEl= document.querySelector(".questiontext");
-var intro= document.querySelector(".quizintro");
-var nextQuestion = document.querySelector(".next");
-
-var answerList = document.querySelector(".answers");
-
-var rightOrWrong = document.querySelector(".rightorwrong");
-
-var questionsAnswers= [
-   {question1: "Which of these is a coding language?", 
-   possibleAnswers: ["HTML"==false, "Javascript"==true, "CSS"==false, "All of the Above"==false]
+//Question & Answer Object
+const questions = [
+   {question: "Which of these is a coding language?", 
+   answers: [
+      {text: "HTML", correct: false},
+      {text: "Javascript", correct: true},
+      {text: "CSS", correct: false},
+      {text: "All of the above", correct: false},
+   ]
 },
 
-{question2: "What is your name?",
-possibleAnswers: ["hoo"==true, "moo"==false, "floo"==false, "shoo"==false]
+{question: "Which of these identifiers should be placed in front of JQUERY statements?", 
+   answers: [
+      {text: "$", correct: true},
+      {text: "+", correct: false},
+      {text: "@", correct: false},
+      {text: "%", correct: false},
+   ]
 },
+
+{question: "What built-in Javascript function allows me to set a timer?", 
+   answers: [
+      {text: "toString", correct: false},
+      {text: ".combine", correct: false},
+      {text: "parseInt", correct: false},
+      {text: "setInterval()", correct: true},
+   ]
+}
+
 
 ];
 
+//Variables created for HTML elements
+const questionEl = document.getElementById("question");
+const answerButton = document.getElementById("answer-options");
+const nextButton = document.getElementById("next-btn");
+const answerPrompt = document.getElementById("rightorwrong");
+const timerEl = document.querySelector(".timer");
 
+//Setting initial scores and indices
+let questionIndex = 0;
+let timeLeft = 75;
 
+function setTime() {
+   // Sets interval in variable
+   var timerInterval = setInterval(function() {
+     timeLeft--;
+     timerEl.textContent = timeLeft;
+ 
+     if(timeLeft === 0) {
+       // Stops execution of action at set interval
+       clearInterval(timerInterval);
+       // Calls function to create and append image
+       
+     }
+ 
+   }, 1000);
+ }
+//Function to intitiate quiz
+function startQuiz() {
+ questionIndex = 0;
+setTime();
+ nextButton.innerHTML = "Next";
+ showQuestion()  
+}
 
-var startQuiz = function() {
-   questionEl.textContent = questionsArray[0];
+//Function to show questions in the question index
+function showQuestion() {
+   //Resets previous question and answers
+   resetState();
+   let currentQuestion = questions[questionIndex];
+   let questionNo = questionIndex + 1;
+   questionEl.innerHTML = questionNo + "." + currentQuestion.question;
    
-   for(var i=0; i < questionsAnswers.length; i++) {
-    answerList.textContent += answersOneArray[i];
-    console.log(answersOneArray[i]);
-   }
-   correctAnswer = answersOneArray[1];
-
-
+//Creates button to add answers to
+currentQuestion.answers.forEach(answer => {
+ const button = document.createElement("button");
+ button.innerHTML = answer.text;
+ button.classList.add("btn");
+ answerButton.appendChild(button);
+ if(answer.correct){
+   button.dataset.correct=answer.correct;
+   
+ }
+ //Event listener to listen for clicked answer
+ button.addEventListener("click", selectAnswer);
+});
+}
+function resetState(){
+   answerPrompt.style.display = "none";
+   console.log(answerButton.children)
+  while(answerButton.firstChild){
+   console.log(answerButton.firstChild )
+     answerButton.removeChild(answerButton.firstChild);
    } 
-   
+   console.log(answerButton.children )
+   console.log(answerButton.children.length); 
+   // //for (var i = 0; i<answerButton.children.length; i+=1) { 
+   //  //  console.log(answerButton.children[i]);
+   //   answerButton.removeChild(answerButton.children[0]);
+   //   answerButton.removeChild(answerButton.children[1]);
+   //   answerButton.removeChild(answerButton.children[2]);
+   //   answerButton.removeChild(answerButton.children[3]);
+
+   // }
+}
+//Displays answer is correct or incorrecr and goes to next question
+function selectAnswer(event){
+    selectedBtn = event.target;
+   const isCorrect = selectedBtn.dataset.correct === "true";
+   if (isCorrect) {
+      $(answerPrompt).show();
+      answerPrompt.textContent = "Answer Correct!!!!!";
+   } else {
+      $(answerPrompt).show();
+    answerPrompt.textContent = "Answer Incorrect 😯";
+    //Subtracts 5 from score for every wrong answer
+    timeLeft-=5
+   }
+   setTimeout(nextQuestion, 500); 
+}
+//Resets question element and displays score
+function showScore(){
+   resetState();
+   clearInterval();
+   if (timeLeft<0) {
+      timeLeft=0;}
+   questionEl.innerHTML = "You scored " + timeLeft + "! Save your high score by entering your name below!";
+   const highScoreInput = document.createElement("input");
+   highScoreInput.classList.add("inputField");
+
+   answerButton.appendChild(highScoreInput);
+
+}
+function nextQuestion(){
+   questionIndex++;
+   if(questionIndex < questions.length){
+      showQuestion();
+   } else {
+      showScore();
+   }
+}
 
 
-buttonEl.addEventListener("click", startQuiz);
+
+startQuiz();
